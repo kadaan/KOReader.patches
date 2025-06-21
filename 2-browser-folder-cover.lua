104a439bf36118a169f6755f8aab90f3f27a5e89
sebdelsol <seb.morin@gmail.com>
2025-06-21 23:33:01 +0200

    clean

--- 2-browser-folder-cover.lua
+++ 2-browser-folder-cover.lua
@@ -2,7 +2,6 @@ local AlphaContainer = require("ui/widget/container/alphacontainer")
 local BD = require("ui/bidi")
 local BottomContainer = require("ui/widget/container/bottomcontainer")
 local CenterContainer = require("ui/widget/container/centercontainer")
-local Device = require("device")
 local Font = require("ui/font")
 local FrameContainer = require("ui/widget/container/framecontainer")
 local ImageWidget = require("ui/widget/imagewidget")
@@ -21,12 +20,18 @@ local function capitalize(sentence)
     return table.concat(words, " ")
 end
 
-userpatch.registerPatchPluginFunc("coverbrowser", function(plugin)
+local function getAlphaFrame(border_size, alpha, widget)
+    return FrameContainer:new {
+        padding = 0,
+        bordersize = border_size,
+        AlphaContainer:new { alpha = alpha, widget },
+    }
+end
+
+local function patchCoverbrowser(plugin)
     local MosaicMenu = require("mosaicmenu")
     local MosaicMenuItem = userpatch.getUpValue(MosaicMenu._updateItemsBuildUI, "MosaicMenuItem")
-    if not MosaicMenuItem then return end
     local BookInfoManager = userpatch.getUpValue(MosaicMenuItem.update, "BookInfoManager")
-    if not BookInfoManager then return end
     local original_update = MosaicMenuItem.update
 
     function MosaicMenuItem:update(...)
@@ -35,24 +40,24 @@ userpatch.registerPatchPluginFunc("coverbrowser", function(plugin)
             return
         end
 
-        local is_directory = not (self.entry.is_file or self.entry.file)
-        if not is_directory then return end
+        if self.entry.is_file or self.entry.file then return end -- it's a file
 
         local dir_path = self.entry and self.entry.path
-        if dir_path then
-            self.menu._dummy = true
-            local entries = self.menu:genItemTableFromPath(dir_path) -- sorted
-            self.menu._dummy = false
+        if not dir_path then return end
 
-            for _, entry in ipairs(entries) do
-                local bookinfo = BookInfoManager:getBookInfo(entry.path, true)
-                local widget = self:_getFolderCover(bookinfo)
-                if widget then
-                    if self._underline_container[1] then self._underline_container[1]:free(true) end
-                    self._underline_container[1] = widget
-                    self._foldercover_processed = true
-                    break
-                end
+        self.menu._dummy = true
+        local entries = self.menu:genItemTableFromPath(dir_path) -- sorted
+        self.menu._dummy = false
+        if not entries then return end
+
+        for _, entry in ipairs(entries) do
+            local bookinfo = BookInfoManager:getBookInfo(entry.path, true)
+            local widget = self:_getFolderCover(bookinfo)
+            if widget then
+                if self._underline_container[1] then self._underline_container[1]:free(true) end
+                self._underline_container[1] = widget
+                self._foldercover_processed = true
+                break
             end
         end
     end
@@ -60,9 +65,9 @@ userpatch.registerPatchPluginFunc("coverbrowser", function(plugin)
     function MosaicMenuItem:_getFolderCover(bookinfo)
         if
             bookinfo
-            and not bookinfo.ignore_cover
-            and bookinfo.cover_fetched
             and bookinfo.has_cover
+            and bookinfo.cover_fetched
+            and not bookinfo.ignore_cover
             and not BookInfoManager.isCachedCoverInvalid(bookinfo, self.menu.cover_specs)
         then
             local border_size = Size.border.thick
@@ -76,63 +81,43 @@ userpatch.registerPatchPluginFunc("coverbrowser", function(plugin)
             )
             local image = ImageWidget:new { image = bookinfo.cover_bb, scale_factor = scale_factor }
             image:_render()
-            local image_size = image:getSize()
-            local img_dimen = { w = image_size.w + 2 * border_size, h = image_size.h + 2 * border_size }
-            local directory, nbitems = self:_getTextBoxes(img_dimen, border_size)
+            local img_size = image:getSize()
+            local dimen = { w = img_size.w + 2 * border_size, h = img_size.h + 2 * border_size }
+            local directory, nbitems = self:_getTextBoxes { w = img_size.w, h = img_size.h }
+
             return CenterContainer:new {
                 dimen = { w = self.width, h = self.height },
                 OverlapGroup:new {
-                    dimen = img_dimen,
-                    FrameContainer:new {
-                        width = img_dimen.w,
-                        height = img_dimen.h,
-                        padding = 0,
-                        bordersize = border_size,
-                        image,
-                    },
-                    CenterContainer:new {
-                        dimen = img_dimen,
-                        FrameContainer:new {
-                            padding = 0,
-                            bordersize = border_size,
-                            AlphaContainer:new { alpha = alpha, directory },
-                        },
-                    },
-                    BottomContainer:new {
-                        dimen = img_dimen,
-                        FrameContainer:new {
-                            padding = 0,
-                            bordersize = border_size,
-                            AlphaContainer:new { alpha = alpha, nbitems },
-                        },
-                    },
+                    dimen = dimen,
+                    FrameContainer:new { padding = 0, bordersize = border_size, image },
+                    CenterContainer:new { dimen = dimen, getAlphaFrame(border_size, alpha, directory) },
+                    BottomContainer:new { dimen = dimen, getAlphaFrame(border_size, alpha, nbitems) },
                 },
             }
         end
     end
 
-    function MosaicMenuItem:_getTextBoxes(dimen, border_size)
-        local dimen_in = { w = dimen.w - border_size * 2, h = dimen.h - border_size * 2 }
-
+    function MosaicMenuItem:_getTextBoxes(dimen)
         local nbitems = TextBoxWidget:new {
             text = self.mandatory,
             face = Font:getFace("cfont", 17),
-            width = dimen_in.w,
+            width = dimen.w,
             alignment = "center",
         }
 
         local text = self.text
-        if text:match("/$") then text = text:sub(1, -2) end -- remove /
+        if text:match("/$") then text = text:sub(1, -2) end -- remove "/"
         text = BD.directory(capitalize(text))
-        local available_height = dimen_in.h - 2 * nbitems:getSize().h
+        local available_height = dimen.h - 2 * nbitems:getSize().h
         local dir_font_size = 25
         local directory
+
         while true do
             if directory then directory:free(true) end
             directory = TextBoxWidget:new {
                 text = text,
                 face = Font:getFace("cfont", dir_font_size),
-                width = dimen_in.w,
+                width = dimen.w,
                 alignment = "center",
                 bold = true,
             }
@@ -147,6 +132,9 @@ userpatch.registerPatchPluginFunc("coverbrowser", function(plugin)
                 break
             end
         end
+
         return directory, nbitems
     end
-end)
+end
+
+userpatch.registerPatchPluginFunc("coverbrowser", patchCoverbrowser)
