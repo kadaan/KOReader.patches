5dc8635ae3aab622c6ee71b49eb126867490b48e
sebdelsol <seb.morin@gmail.com>
2025-06-28 00:02:56 +0200

    clean

--- 2-browser-hide-underline.lua
+++ 2-browser-hide-underline.lua
@@ -7,7 +7,7 @@ local userpatch = require("userpatch")
 local function patchCoverBrowser(plugin)
     local MosaicMenu = require("mosaicmenu")
     local MosaicMenuItem = userpatch.getUpValue(MosaicMenu._updateItemsBuildUI, "MosaicMenuItem")
-    if not MosaicMenuItem then return end -- Protect against remnants of project title
+    if not MosaicMenuItem then return end -- protect against remnants of project title or ebanDev patches
 
     function MosaicMenuItem:onFocus()
         self._underline_container.color = Blitbuffer.COLOR_WHITE
